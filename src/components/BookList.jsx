4edfd15fcc54e6c5be03ecb9c08e0bd663f8a6b0
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-10 15:26:29 +0100

    BookList is function component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,60 +1,57 @@
-import { Component } from "react"
+import { useState, useEffect } from "react"
 import { Container, Row, Col, Form } from "react-bootstrap"
 import SingleBook from "./SingleBook"
 
-class BookList extends Component {
-  state = {
-    userSearch: "",
-  }
-
-  searchBooks() {
-    const userSearch = this.state.userSearch.toLowerCase()
-    // the books that the match the search pattern 
-    const filteredBooks = this.props.books.filter((book) => {
-        const bookTitle = book.title.toLowerCase()
-        const isBookSearched = bookTitle.includes(userSearch)
-        return isBookSearched
-    })
-
-    return filteredBooks.map((book) => {
-      return (
-        <Col key={book.asin} className="d-flex justify-content-center">
-          <SingleBook book={book} selectedBook={this.props.selectedBook} updateSelectedBook={this.props.updateSelectedBook}  />
-        </Col>
-      )
-    })
-  }
+const BookList = (props) => {
+  const [userSearch, setUserSearch] = useState("")
 
   // ad ogni cambiamento di stato e props, il metodo render del componente viene eseguito
-  render() {
-    return (
-      <Container style={{ marginTop: this.props.marginTop || 0 }}>
-        {/* search */}
-        <Row>
-          <Col>
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Control
-                  type="search"
-                  // freccina sotto -> collego il valore dello stato al valore dell'input
-                  value={this.state.userSearch}
-                  onChange={(event) => {
-                    this.setState({
-                      userSearch: event.target.value,
-                    })
-                  }}
-                  placeholder="Search here..."
-                />
-              </Form.Group>
-            </Form>
-          </Col>
-        </Row>
+  return (
+    <Container style={{ marginTop: props.marginTop || 0 }}>
+      {/* search */}
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="search"
+                // freccina sotto -> collego il valore dello stato al valore dell'input
+                value={userSearch}
+                onChange={(event) => {
+                  setUserSearch(event.target.value)
+                }}
+                placeholder="Search here..."
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
 
-        {/* books */}
-        <Row className="row-cols-1 row-cols-lg-2 g-3">{this.searchBooks()}</Row>
-      </Container>
+      {/* books */}
+      <Row className="row-cols-1 row-cols-lg-2 g-3">{searchBooks({userSearch, props})}</Row>
+    </Container>
+  )
+}
+
+const searchBooks = (componentInfo) => {
+  const { userSearch, props } = componentInfo
+
+  const userSearchLowercase = userSearch.toLowerCase()
+  
+  // the books that the match the search pattern
+  const filteredBooks = props.books.filter((book) => {
+    const bookTitle = book.title.toLowerCase()
+    const isBookSearched = bookTitle.includes(userSearchLowercase)
+    return isBookSearched
+  })
+
+  return filteredBooks.map((book) => {
+    return (
+      <Col key={book.asin} className="d-flex justify-content-center">
+        <SingleBook book={book} selectedBook={props.selectedBook} updateSelectedBook={props.updateSelectedBook} />
+      </Col>
     )
-  }
+  })
 }
 
 export default BookList
