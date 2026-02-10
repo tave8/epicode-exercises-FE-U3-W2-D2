ef9073821a21aedf0a9032aef07029b358f664e6
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-10 15:42:49 +0100

    CommentArea turned into function component

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react"
+import { useState, useEffect } from "react"
 import CommentsList from "./CommentsList"
 import AddComment from "./AddComment"
 import AIBookSummary from "./AIBookSummary"
@@ -7,18 +7,55 @@ import OpenAI from "../classes/OpenAI"
 
 import { Container, Row, Col, Form, Alert } from "react-bootstrap"
 
-class CommentArea extends Component {
-  state = {
-    reviews: [],
-    bookSummary: "",
-    isLoading: true,
-    isLoadingSummary: true,
-  }
+const CommentArea = (props) => {
 
-  getReviews = (bookAsin) => {
-    this.setState({
-      isLoading: true,
-    })
+  const [reviews, setReviews] = useState([])
+  const [bookSummary, setBookSummary] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
+  const [isLoadingSummary, setIsLoadingSummary] = useState(true)
+
+  useEffect(() => {
+    // make sure the book exists
+    if (props.selectedBook) {
+      getReviews({ setIsLoading, setReviews })(props.selectedBook.asin)
+      getBookSummaryFromTitle({ setIsLoadingSummary, setBookSummary })(props.selectedBook.title)
+    }
+  }, [props.selectedBook])
+
+
+  return (
+    <>
+      {/* no book selected */}
+      {!props.bookIsSelected && (
+        <Alert variant="info">
+          <p>No book selected yet</p>
+        </Alert>
+      )}
+
+      {/* book selected */}
+      {props.bookIsSelected && (
+        <>
+          <h2>{props.selectedBook.title}</h2>
+
+          {/* AI summary */}
+          <AIBookSummary isLoading={isLoadingSummary} bookSummary={bookSummary} />
+
+          {/* leave your review */}
+          <AddComment book={props.selectedBook} getReviews={getReviews({ props, setIsLoading, setReviews })} />
+
+          {/* reviews for this book */}
+          <CommentsList book={props.selectedBook} reviews={reviews} isLoading={isLoading} />
+        </>
+      )}
+    </>
+  )
+}
+
+const getReviews = (componentInfo) => {
+  const { setIsLoading, setReviews } = componentInfo
+
+  return (bookAsin) => {
+    setIsLoading(true)
 
     const url = `https://striveschool-api.herokuapp.com/api/comments/${bookAsin}`
     const config = {
@@ -38,20 +75,20 @@ class CommentArea extends Component {
       })
       .then((data) => {
         const reviews = data
-        this.setState({
-          reviews,
-          isLoading: false,
-        })
+        setReviews(reviews)
+        setIsLoading(false)
       })
       .catch((err) => {
         console.error(err)
-        this.setState({
-          isLoading: false,
-        })
+        setIsLoading(false)
       })
   }
+}
 
-  getBookSummaryFromTitle = async (bookTitle) => {
+const getBookSummaryFromTitle = (componentInfo) => {
+  const { setIsLoadingSummary, setBookSummary } = componentInfo
+
+  return async (bookTitle) => {
     // console.log(bookTitle)
     const prompt =
       `I give you the title of a book.` +
@@ -62,78 +99,18 @@ class CommentArea extends Component {
       `${bookTitle}` +
       `"""`
     try {
-      this.setState({ isLoadingSummary: true })
+      setIsLoadingSummary(true)
 
       const openai = new OpenAI({ simplify: true })
       const data = await openai.ask(prompt)
       const aiSummary = data.message
 
-      this.setState({
-        bookSummary: aiSummary,
-      })
-
-      this.setState({ isLoadingSummary: false })
+      setBookSummary(aiSummary)
+      setIsLoadingSummary(false)
     } catch (err) {
       console.error(err)
-      this.setState({ isLoadingSummary: false })
+      setIsLoadingSummary(false)
     }
-  }
-
-  componentDidMount() {
-    // this.getReviews(this.props.book.asin)
-  }
-
-  // eslint-disable-next-line no-unused-vars
-  componentDidUpdate(prevProps, prevState) {
-    const prevSelectedBook = prevProps.selectedBook
-    const currSelectedBook = this.props.selectedBook
-    let selectedBookWasChanged = false
-
-    // before null, after book
-    if (prevSelectedBook === null) {
-      selectedBookWasChanged = currSelectedBook !== null
-    }
-    // before book, after another book
-    else {
-      selectedBookWasChanged = prevSelectedBook.asin !== currSelectedBook.asin
-    }
-
-    if (selectedBookWasChanged) {
-      // this.updateSelectedBook()
-      // console.log("selected book was changed")
-      // console.log(prevProps, this.props)
-      this.getReviews(this.props.selectedBook.asin)
-      this.getBookSummaryFromTitle(this.props.selectedBook.title)
-    }
-  }
-
-  render() {
-    return (
-      <>
-        {/* no book selected */}
-        {!this.props.bookIsSelected && (
-          <Alert variant="info">
-            <p>No book selected yet</p>
-          </Alert>
-        )}
-
-        {/* book selected */}
-        {this.props.bookIsSelected && (
-          <>
-            <h2>{this.props.selectedBook.title}</h2>
-
-            {/* AI summary */}
-            <AIBookSummary isLoading={this.state.isLoadingSummary} bookSummary={this.state.bookSummary} />
-
-            {/* leave your review */}
-            <AddComment book={this.props.selectedBook} getReviews={this.getReviews} />
-
-            {/* reviews for this book */}
-            <CommentsList book={this.props.selectedBook} reviews={this.state.reviews} isLoading={this.state.isLoading} />
-          </>
-        )}
-      </>
-    )
   }
 }
 
